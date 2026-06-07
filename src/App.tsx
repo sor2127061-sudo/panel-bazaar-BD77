4f110d8d642fda63008b12210bbea0da46157174
sor2127061-sudo <sor2127061@gmail.com>
2026-06-07 21:35:51 +0600

    feat: wrap App with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-/**
- * @license SPDX-License-Identifier: Apache-2.0
- */
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 import LumenShell from './components/LumenShell';
 import Navbar from './components/Navbar';
@@ -26,30 +24,32 @@ import AdminDashboard from './pages/AdminDashboard';
 
 export default function App() {
   return (
-    <ToastProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <LumenShell>
-            <Navbar />
-            <Routes>
-              <Route path="/"              element={<Home />} />
-              <Route path="/login"         element={<Login />} />
-              <Route path="/register"      element={<Register />} />
-              <Route path="/topup/:id"     element={<ProductDetail />} />
-              <Route path="/bundles"       element={<Bundles />} />
-              <Route path="/add-fund"      element={<ProtectedRoute><AddFund /></ProtectedRoute>} />
-              <Route path="/topup-success" element={<ProtectedRoute><TopupSuccess /></ProtectedRoute>} />
-              <Route path="/orders"        element={<ProtectedRoute><Orders /></ProtectedRoute>} />
-              <Route path="/codes"         element={<ProtectedRoute><MyKeys /></ProtectedRoute>} />
-              <Route path="/account"       element={<ProtectedRoute><Account /></ProtectedRoute>} />
-              <Route path="/stock-request" element={<ProtectedRoute><StockRequest /></ProtectedRoute>} />
-              <Route path="/admin"         element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-              <Route path="*"             element={<Navigate to="/" replace />} />
-            </Routes>
-            <BottomNav />
-          </LumenShell>
-        </BrowserRouter>
-      </AuthProvider>
-    </ToastProvider>
+    <ThemeProvider>
+      <ToastProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <LumenShell>
+              <Navbar />
+              <Routes>
+                <Route path="/"              element={<Home />} />
+                <Route path="/login"         element={<Login />} />
+                <Route path="/register"      element={<Register />} />
+                <Route path="/topup/:id"     element={<ProductDetail />} />
+                <Route path="/bundles"       element={<Bundles />} />
+                <Route path="/add-fund"      element={<ProtectedRoute><AddFund /></ProtectedRoute>} />
+                <Route path="/topup-success" element={<ProtectedRoute><TopupSuccess /></ProtectedRoute>} />
+                <Route path="/orders"        element={<ProtectedRoute><Orders /></ProtectedRoute>} />
+                <Route path="/codes"         element={<ProtectedRoute><MyKeys /></ProtectedRoute>} />
+                <Route path="/account"       element={<ProtectedRoute><Account /></ProtectedRoute>} />
+                <Route path="/stock-request" element={<ProtectedRoute><StockRequest /></ProtectedRoute>} />
+                <Route path="/admin"         element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+                <Route path="*"             element={<Navigate to="/" replace />} />
+              </Routes>
+              <BottomNav />
+            </LumenShell>
+          </BrowserRouter>
+        </AuthProvider>
+      </ToastProvider>
+    </ThemeProvider>
   );
 }
